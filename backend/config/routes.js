58eded191aa1ce792ef0b2a06703d5676c7dc29d
Difusão <difusao@gmail.com>
2021-12-17 18:06:26 -0300

    Backend

---
+++ backend/config/routes.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const auth = require('./auth')
+
+module.exports = function(server) {
+
+    /*
+     * Rotas abertas
+     */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
+
+    /*
+     * Rotas protegidas por Token JWT
+     */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
+
+
+}
